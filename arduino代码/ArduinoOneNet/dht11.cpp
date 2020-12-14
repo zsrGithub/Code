3a556a97afff44295f569d337f7f7c33b3bc55a8
zsrGithub <75732966+zsrGithub@users.noreply.github.com>
2020-12-14 10:25:18 +0800

    Update dht11.cpp

--- arduino代码/ArduinoOneNet/dht11.cpp
+++ arduino代码/ArduinoOneNet/dht11.cpp
@@ -1,30 +1,5 @@
-//
-//    FILE: dht11.cpp
-// VERSION: 0.4.1
-// PURPOSE: DHT11 Temperature & Humidity Sensor library for Arduino
-// LICENSE: GPL v3 (http://www.gnu.org/licenses/gpl.html)
-//
-// DATASHEET: http://www.micro4you.com/files/sensor/DHT11.pdf
-//
-// HISTORY:
-// George Hadjikyriacou - Original version (??)
-// Mod by SimKard - Version 0.2 (24/11/2010)
-// Mod by Rob Tillaart - Version 0.3 (28/03/2011)
-// + added comments
-// + removed all non DHT11 specific code
-// + added references
-// Mod by Rob Tillaart - Version 0.4 (17/03/2012)
-// + added 1.0 support
-// Mod by Rob Tillaart - Version 0.4.1 (19/05/2012)
-// + added error codes
-//
-
 #include "dht11.h"
 
-// Return values:
-// DHTLIB_OK
-// DHTLIB_ERROR_CHECKSUM
-// DHTLIB_ERROR_TIMEOUT
 int dht11::read(int pin)
 {
 	// BUFFER TO RECEIVE
